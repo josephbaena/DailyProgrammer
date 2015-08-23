8aafc8cb8ca5a537c44f0b6cd23ab8c8b14a2818
Joseph Baena <jbaena@stanford.edu>
2015-08-22 19:06:09 -0700

    done AlphabeticalLetters

---
+++ src/AlphabeticalLetters.java
@@ -0,0 +1,49 @@
+/**
+ * Created by josephbaena on 8/22/15.
+ */
+public class AlphabeticalLetters {
+
+    public static void main(String[] args) {
+        String input = args[0];
+
+        // convert to upper case if necessary
+        String inputWord = input.toUpperCase();
+        boolean isInOrder = inAlphabeticalOrder(inputWord);
+        if (isInOrder) {
+            System.out.println(input + " IN ORDER");
+        } else {
+            boolean isInReverseOrder = inReverseOrder(inputWord);
+            if (isInReverseOrder) {
+                System.out.println(input + " REVERSE ORDER");
+            } else {
+                System.out.println(input + " NOT IN ORDER");
+            }
+        }
+    }
+
+    private static boolean inAlphabeticalOrder(String inputWord) {
+        char lastChar = 'A';
+        int inputLength = inputWord.length();
+        for (int i = 0; i < inputLength; i++) {
+            char ch = inputWord.charAt(i);
+            if (ch < lastChar) {
+                return false;
+            }
+            lastChar = ch;
+        }
+        return true;
+    }
+
+    private static boolean inReverseOrder(String inputWord) {
+        char lastChar = 'Z';
+        int inputLength = inputWord.length();
+        for (int i = 0; i < inputLength; i++) {
+            char ch = inputWord.charAt(i);
+            if (ch > lastChar) {
+                return false;
+            }
+            lastChar = ch;
+        }
+        return true;
+    }
+}
