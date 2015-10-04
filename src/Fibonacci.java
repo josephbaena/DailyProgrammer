3eb80a4da0d01204aee44d6524829896144796e4
Joseph Baena <jbaena@stanford.edu>
2015-10-04 13:57:31 -0700

    implementation of iterative fibonacci

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
  */
 public class Fibonacci {
 
+    // O(2^n) implementation
     public static int recFibonacci(int n) {
         if (n <= 1) {
             return n;
@@ -13,12 +14,32 @@ public class Fibonacci {
         return recFibonacci(n - 1) + recFibonacci(n - 2);
     }
 
+    // iterative implementation
+    public static int iterFibonacci(int n) {
+        if ((n == 0) || (n == 1)) return n; // 0 or 1
+
+        // at this point, n >= 2
+        int x1 = 0;
+        int x2 = 1;
+        int sum = 0;
+
+        for (int i = 2; i <= n; i++) {
+            sum = x1 + x2;
+            x1 = x2;
+            x2 = sum;
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int lengthOfSeries = Integer.parseInt(reader.readLine());
 
+        assert(lengthOfSeries >= 0);
+
         for (int i = 1; i <= lengthOfSeries; i++) {
-            System.out.println(recFibonacci(i) + "\t");
+            System.out.println(iterFibonacci(i) + "\t");
         }
     }
 }
